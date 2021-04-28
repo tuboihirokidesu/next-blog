b1f0f69eb9bfeded90057064cc66b9026ca5d83f
tuboihirokidesu <tuboi.hiroki@icloud.com>
2021-04-28 14:18:55 +0900

    title change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,9 +43,6 @@ const Home: NextPage<Props> = ({ article }) => {
 
   return (
     <div className="px-5 py-2 overflow-scroll">
-      <Head>
-        <title>Homepage</title>
-      </Head>
       <div className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
         {/* // do this div style later (after putting the content) */}
         <div className="h-full col-span-12 p-4 text-base text-center bg-white dark:bg-black lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
